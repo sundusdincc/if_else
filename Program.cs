454a88e498d9f83de3df712354ca475a6542b6e6
Sündüs Dinç <sundus.dinc01@hotmail.com>
2022-11-04 15:27:22 +0300

    if-else

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int time = DateTime.Now.Hour;
+        if (time >= 6 && time <= 11)
+        {
+            Console.WriteLine("Günaydın!");
+        }
+        else if (time <= 18)
+        {
+            Console.WriteLine("İyi Günler!");
+        }
+        else
+        {
+            Console.WriteLine("İyi Geceler!");
+        }
+
+
+        //String sonuc=time<=18 ? "İyi Günler!" : "İyi Geceler!";
+        String sonuc = time >= 6 && time <= 11 ? "Günaydın!" : time <= 18 ? "İyi Günler!" : "İyi Geceler";
+        Console.WriteLine(sonuc);
+
+
+        int a = 78;
+        String grade = a < 50 ? "Student is failed" : a >= 50 && a <= 60 ? "Student gets D grade" : a >= 70 && a <= 80 ? "Student gets B grade" : "Student gets A+ grade";
+        Console.WriteLine(grade);
+        Console.ReadKey();
+    }
+}
